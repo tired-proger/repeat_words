815359fdd7da02695eb417caa12b60fab99f2aee
Matvey <85248518+tired-proger@users.noreply.github.com>
2025-05-01 23:45:41 +0500

    Update points.js

--- src/api/points.js
+++ src/api/points.js
@@ -55,6 +55,7 @@ function editDescriptionWord(data) {
             closeDialog();
             dispatch(toggleLoaderVisibility(true));
             const response = await api.post("description", { id, text }, addHeaders(login, pass));
+            console.log(response.statusText !== "OK");
             if (response.statusText !== "OK") throw new Error("Ошибка");
             
             dispatch(editDescription({ id, content: text }));
